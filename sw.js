df5dac406c795e64709086ac8afebc7b9db36288
mrpoffice <mrpoffice@gmail.com>
2025-11-28 13:48:17 -0600

    Simple CSS paper-fold cover instead of Lottie
    - Removed Lottie player dependency
    - Clean paper-fold style cover with CSS
    - Dog-ear corner detail
    - "tap to peek" text
    - Much more reliable!

--- sw.js
+++ sw.js
@@ -1,10 +1,9 @@
-const CACHE_NAME = 'jotto-v6';
+const CACHE_NAME = 'jotto-v7';
 const urlsToCache = [
   '/',
   '/index.html',
   '/manifest.json',
-  '/icon-192.svg',
-  '/cover.json'
+  '/icon-192.svg'
 ];
 
 // Install service worker and cache files
